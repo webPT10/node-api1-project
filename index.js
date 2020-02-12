036f997829e09d0fc5b29f3e8d97ca5641813c2d
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-02-12 15:25:37 -0800

    .POST completed

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
-// implement your API here
-const http = require('http'); // built in node.js module to handle http traffic
+let db = require("./data/db");
 
-const hostname = '127.0.0.1'; // the local computer where the server is running
-const port = 4000; // a port we'll use to watch for traffic
+const express = require("express"); // import Express package
 
-const server = http.createServer((req, res) => {
-  // creates our server
-  res.statusCode = 200; // http status code returned to the client
-  res.setHeader('Content-Type', 'text/plain'); // inform the client that we'll be returning text
-  res.end('Hello World from Eric \n'); // end the request and send a response with the specified message
+const server = express(); // creates the server
+server.use(express.json());
+
+let newId = 0;
+
+server.get("/", (requested, response) => {
+  response.send("Nil Satis Nisi Optimum");
 });
 
-server.listen(port, hostname, () => {
-  // start watching for connections on the port specified
-  console.log(`Server running at http://${hostname}:${port}/`);
+// POST
+server.post("/api/users", (request, response) => {
+  const { name, bio } = request.body;
+  const newUser = { name, bio, id: Date.now() };
+
+  if (!name || !bio) {
+    response.status(400).json({
+      errorMessage: "Please provide name and bio for the User."
+    });
+  }
+
+  db.insert(newUser)
+    .then(user => {
+      response.status(201).json(user);
+    })
+    .catch(error => {
+      response.status(500).json({
+        error: error,
+        message:
+          "There was a terrible error while saving the user to the database."
+      });
+    });
+});
+
+// GET
+// returns ARRAY of all users objects in database
+server.get("/api/users", (request, response) => {});
+
+// returns user OBJECT with specific id
+server.get("/api/users/:id", (request, response) => {});
+
+// PUT
+// updates user with specific ID from request body > returns modified, not original
+server.put("/api/users/:id", (request, response) => {});
+
+// DELETE
+// removes user with specific id & returns deleted user
+server.delete("/api/users/:id", (request, response) => {
+  const user = users.find(u => u.id == request.params.id);
+});
+
+const port = 4040;
+server.listen(port, () => {
+  console.log(`server started @ http://localhost:${port}`);
 });
